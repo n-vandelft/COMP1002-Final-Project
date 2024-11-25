134dbb90105ca348bc5538a7d2d5c1387f5c7c56
MachinistHamilton <61161498+MachinistHamilton@users.noreply.github.com>
2024-11-25 10:36:30 -0500

    Update header-footer.js

--- FinalProject-with-Global/JS/header-footer.js
+++ FinalProject-with-Global/JS/header-footer.js
@@ -8,7 +8,8 @@ document.addEventListener('DOMContentLoaded', () => {
             <ul>
                 <li><a href="index.html">Home</a></li>
                 <li><a href="products.html">Products</a></li>
-                <li><a href="services.html">Services</a></li>
+                <li><a href="services.html">About</a></li>
+                <li><a href="services.html">Contact</a></li>
             </ul>
         </nav>`;
     head1.innerHTML = myHeader;
