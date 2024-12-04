c661a5593bc3730d501e08f8c49cad668f893eec
n-vandelft <176562037+n-vandelft@users.noreply.github.com>
2024-12-04 13:38:43 -0500

    Pulled files out of Final Project directory

---
+++ js/header-footer.js
@@ -0,0 +1,38 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Populate Header
+    const head1 = document.getElementById("header1");
+    const myHeader = `
+        <nav>
+            <div class="menu-icon" id="mobile-menu">☰</div>
+            <a href="index.html"><img src="images/nintendologo.png" alt="site logo"></a>
+            <ul class="nav-list" id="nav-list">
+                <li><a href="index.html">Home</a></li>
+                <li><a href="products.html">Products</a></li>
+                <li><a href="index.html#contact">Contact</a></li>
+                <li><a href="about.html">About</a></li>
+            </ul>
+        </nav>`;
+    head1.innerHTML = myHeader;
+
+    // Mobile menu toggle
+    const mobileMenu = document.getElementById('mobile-menu');
+    const navList = document.getElementById('nav-list');
+    mobileMenu.addEventListener('click', () => {
+        navList.classList.toggle('active');
+    });
+
+    // Populate Footer
+    const footer1 = document.getElementById("footer1");
+    const myFooter = `
+        <ul>
+            <li><a href="about.html">About Us</a></li>
+            <li><a href="index.html#contact">Contact Us</a></li>
+        </ul>
+        <div>
+            <p>&copy; <span id="year"></span> Nintendo. All rights reserved.</p>
+        </div>`;
+    footer1.innerHTML = myFooter;
+
+    // Populate year dynamically
+    document.getElementById("year").textContent = new Date().getFullYear();
+});
